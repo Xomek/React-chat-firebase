5a5974fdfd1c6bae6d373e7a2101935e54d3383e
Xomek <z_v_m_x@mail.ru>
2023-09-25 16:48:24 +0700

    fix: add channel button

--- src/pages/Chat/components/Sidebar/Sidebar.tsx
+++ src/pages/Chat/components/Sidebar/Sidebar.tsx
@@ -48,7 +48,9 @@ const Sidebar: React.FC = () => {
       </div>
 
       <div className={styles.actions}>
-        <div className={styles.plusIcon} onClick={createChannel} />
+        <Button onClick={createChannel}>
+          <div className={styles.plusIcon} />
+        </Button>
       </div>
 
       {loading ? (
